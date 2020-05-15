760e8ae3af1bc06b911e40eae7d47668fb46d68a
KarimElghamry <Karimelghamry@gmail.com>
2020-05-15 15:33:56 +0200

    helper functions renaming

--- src/helpers/complexOperations.js
+++ src/helpers/complexOperations.js
@@ -1,22 +1,21 @@
-export function add({ real1, imag1, real2, imag2 }) {
-    const realPart = real1 +real2;
-    const imaginaryPart = imag1 + imag2;
-    return { real: realPart, imaginary: imaginaryPart };
+export function addComplex({real1, imag1, real2, imag2}) {
+  const realPart = real1 + real2;
+  const imaginaryPart = imag1 + imag2;
+  return {real: realPart, imaginary: imaginaryPart};
 }
 
-export function subtract({ real1, imag1, real2, imag2 }) {
-    const realPart = real1 - real2;
-    const imaginaryPart = imag1 - imag2;
-    return { real: realPart, imaginary: imaginaryPart };
+export function subtractComplex({real1, imag1, real2, imag2}) {
+  const realPart = real1 - real2;
+  const imaginaryPart = imag1 - imag2;
+  return {real: realPart, imaginary: imaginaryPart};
 }
-export function multiplication({ magnitude1, phase1, magnitude2, phase2 }) {
-    const magnitude = magnitude1 * magnitude2;
-    const phase = phase1 + phase2;
-    return { magnitude: magnitude, phase: phase };
+export function multiplicationPhasor({magnitude1, phase1, magnitude2, phase2}) {
+  const magnitude = magnitude1 * magnitude2;
+  const phase = phase1 + phase2;
+  return {magnitude: magnitude, phase: phase};
 }
-export function division({ magnitude1, phase1, magnitude2, phase2 }) {
-    const magnitude = magnitude1 / magnitude2;
-    const phase = phase1 - phase2;
-    return { magnitude: magnitude, phase: phase };
+export function divisionPhasor({magnitude1, phase1, magnitude2, phase2}) {
+  const magnitude = magnitude1 / magnitude2;
+  const phase = phase1 - phase2;
+  return {magnitude: magnitude, phase: phase};
 }
-
