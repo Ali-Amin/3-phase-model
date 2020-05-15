c546b1d224ccb92071e0f584900ce64b4a3f1b27
KarimElghamry <Karimelghamry@gmail.com>
2020-05-15 21:48:06 +0200

    added needed props for output

--- src/components/Outputs.js
+++ src/components/Outputs.js
@@ -3,12 +3,15 @@ import {Row} from 'antd';
 
 import systems from '../assets';
 
-function Outputs({system}) {
+function Outputs({system, voltageValue, ImpedanceTRValue, ImpedanceLoadValue}) {
   return (
     <div>
       <Row>
         {console.log(system)}
         <img alt="" src={systems[system]} width={540} height={350} />
+      </Row>
+      <Row>
+        <button>sdsd</button>
       </Row>
     </div>
   );
