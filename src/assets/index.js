ca53f61bd15f6e02026a6aaf3dc199e930ea6ec4
mrottimista <adel.mohamed855@gmail.com>
2020-05-15 18:20:52 +0200

    add: container of the img

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import Delta_Delta from "./Delta_Delta.png";
+import Delta_Star from "./Delta_Star.png";
+import Star_Delta from "./Star_Delta.png";
+import Star_Star from "./Star_Star.png";
+
+export default {
+  Delta_Delta,
+  Delta_Star,
+  Star_Delta,
+  Star_Star,
+};
