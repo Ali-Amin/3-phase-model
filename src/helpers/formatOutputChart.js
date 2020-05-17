77363be10aaa68d8cacf33901cde7ab1ec1e9de9
mrottimista <adel.mohamed855@gmail.com>
2020-05-18 01:10:52 +0200

    refactor: avoid null cases

--- src/helpers/formatOutputChart.js
+++ src/helpers/formatOutputChart.js
@@ -1,7 +1,10 @@
 const formatInputChart = ({ output, effectOnKey, effectOn }) => {
   let result = output[effectOnKey][effectOn];
-  if (Object.keys(result).length !== 2) {
+  if (result && Object.keys(result).length !== 2) {
     result = result[Object.keys(result)[0]];
+  }
+  if(!result){
+    result={magnitude:0,phase:0}
   }
   return result
 };
